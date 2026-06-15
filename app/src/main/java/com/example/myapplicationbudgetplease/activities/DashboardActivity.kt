6fd812ec5eb38ad9c3cd741f24683cfa0c37f0ee
brakingankels <ST10441569@rcconnect.edu.za>
2026-06-15 19:31:33 +0200

    Create login and dashboard screens

---
+++ app/src/main/java/com/example/myapplicationbudgetplease/activities/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplicationbudgetplease.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplicationbudgetplease.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
